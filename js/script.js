9c0a549cdcd771cc788e569f152b97a8bb9462d9
rodeomar <raed.alkhanbashi@gmail.com>
2023-04-07 13:04:19 -0700

    calculating size and topping prices

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
+//Business logic
+
 function pizza(size,toppings) {
   this.size = size
   this.toppings =toppings;
 }
-pizza.
+pizza.prototype.cost = function() {
+  let basePrice = 10;
+  let sizePrice = 0;
+  let toppingsPrice = 0;
+
+    // Calculate size price
+  
+    switch(this.size) {
+    case 'small':
+      sizePrice = 2;
+      break;
+    case 'medium':
+      sizePrice = 4;
+      break;
+    case 'large':
+      sizePrice = 6;
+      break;
+  }
+
+    // Calculate toppings price
+    
+    if (this.toppings.meat) {
+      toppingsPrice += 2;
+    }
+    if (this.toppings.veggies) {
+      toppingsPrice += 1;
+    }
+    if (this.toppings.sauce) {
+      toppingsPrice += 0.5;
+    }
+    if (this.toppings.drinks) {
+      toppingsPrice += 2;
